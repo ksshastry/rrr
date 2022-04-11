c262af382e2b73261f6f44370275783bfd673f54
ksshastry <ksshstry24@gmail.com>
2022-04-11 15:49:12 +0000

    chalo

--- main.tf
+++ main.tf
@@ -8,9 +8,9 @@ terraform {
 
   required_version = ">=0.14.9"
   backend "s3" {
-    bucket = "my-terra-git"
-    key    = "uam/terraform.tfstate"
-    region = "us-east-1"
+    bucket  = "my-terra-git"
+    key     = "uam/terraform.tfstate"
+    region  = "us-east-1"
     profile = "default"
   }
 }
