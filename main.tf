5ef8223ba54a00183d07a835ab0c27a530580ee2
ksshastry <ksshstry24@gmail.com>
2022-04-11 16:54:23 +0000

    hmm

--- main.tf
+++ main.tf
@@ -3,10 +3,4 @@ provider "aws" {
   region = "us-east-1"
 }
 
-  backend "s3" {
-    bucket  = "my-terra-git"
-    key     = "uam/terraform.tfstate"
-    region  = "us-east-1"
-    profile = "default"
-  }
 
